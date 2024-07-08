e93da276e07a7c2617f819cc5b74db7f0b411754
Jaivan87 <165306433+Jaivan87@users.noreply.github.com>
2024-07-08 11:11:50 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -5,10 +5,10 @@
 #centena: 8
 #milhar: 1
 num = int(input('Digite seu numero: '))
-u = num // 1 % 10  #divisão inteira por 1 e tirar o resto da divisão por 10
-d = num // 10 % 10 #divisão inteira por 10 e tirar o resto da divisão por 10
-c = num // 100 % 10 #divisão inteira por 100 e tirar o resto da divisão por 10
-m = num // 1000 % 10 #divisão inteira por 1000 e tirar o resto da divisão por 10
+u = num // 1 % 10  #divisão inteira por 1 e tira o resto da divisão por 10
+d = num // 10 % 10 #divisão inteira por 10 e tira o resto da divisão por 10
+c = num // 100 % 10 #divisão inteira por 100 e tira o resto da divisão por 10
+m = num // 1000 % 10 #divisão inteira por 1000 e tira o resto da divisão por 10
 print('Analisando o número {}'.format(num))
 print('Unidade:{}'.format(u))
 print('Dezena:{}'.format(d))
