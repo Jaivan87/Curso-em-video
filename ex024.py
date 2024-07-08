1e8c813628170d3c1cab3caad170728e7be5db9d
Jaivan87 <165306433+Jaivan87@users.noreply.github.com>
2024-07-08 09:06:57 -0300

    Update ex024.py

--- ex024.py
+++ ex024.py
@@ -6,21 +6,21 @@ print('Santo' in n)'''
 
 def tem_silva(nome):
   """
-  Verifica se o nome completo da pessoa contém "Silva".
+  Verifica se o nome da cidade contém "Santo".
 
   Argumentos:
-    nome: Uma string contendo o nome completo da pessoa.
+    nome: Uma string contendo o nome cidade da pessoa.
 
   Retorna:
-    True se o nome contiver "Silva", False caso contrário.
+    True se o nome contiver "Santo", False caso contrário.
   """
   nome_maiusculo = nome.upper()
   return "SANTO" in nome_maiusculo
 
-nome = input("Digite seu nome completo: ")
+nome = input("Digite o nome de uma cidade: ")
 
 if tem_silva(nome):
   print("Sua cidade contém Santo no nome!")
 else:
-  print("Sua cidade contém Santo no nome.")
+  print("Sua cidade não contém Santo no nome.")
 
